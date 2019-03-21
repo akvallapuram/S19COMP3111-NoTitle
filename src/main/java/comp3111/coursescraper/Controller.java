7a8c2f5b6eced5c26c33e41fc36866de7a491540
Nikhil <nikhilnanda@gmail.com>
2019-03-21 09:24:20 +0800

    Make some tiny change

--- src/main/java/comp3111/coursescraper/Controller.java
+++ src/main/java/comp3111/coursescraper/Controller.java
@@ -79,6 +79,7 @@ public class Controller {
     @FXML
     void findInstructorSfq() {
     	buttonInstructorSfq.setDisable(true);
+    	textAreaConsole.setText(textAreaConsole.getText()+"\n"+textfieldSfqUrl.getText());
     }
 
     @FXML
