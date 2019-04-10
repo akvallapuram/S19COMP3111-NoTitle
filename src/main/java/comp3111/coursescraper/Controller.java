8d2e502565131ba91285653b5098da5edc2a249f
Jeff Zhan <jeffzhan2008@gmail.com>
2019-04-10 13:15:09 +0800

    Select All 1/2 way done
    filtering stuff

--- src/main/java/comp3111/coursescraper/Controller.java
+++ src/main/java/comp3111/coursescraper/Controller.java
@@ -16,7 +16,9 @@ import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.CornerRadii;
 import javafx.geometry.Insets;
 import javafx.scene.paint.Color;
+import javafx.scene.control.CheckBox;
 
+import javax.swing.*;
 import java.util.Random;
 import java.util.List;
 public class Controller {
@@ -72,6 +74,32 @@ public class Controller {
     private Scraper scraper = new Scraper();
 
     @FXML
+    private CheckBox AM;
+
+    @FXML
+    private CheckBox PM;
+    @FXML
+    private CheckBox Mon;
+    @FXML
+    private CheckBox Tues;
+    @FXML
+    private CheckBox Wed;
+    @FXML
+    private CheckBox Thrs;
+    @FXML
+    private CheckBox Fri;
+    @FXML
+    private CheckBox Sat;
+    @FXML
+    private CheckBox CC;
+    @FXML
+    private CheckBox NE;
+    @FXML
+    private CheckBox LT;
+    @FXML
+    private Button SelectAll;
+
+    @FXML
     void allSubjectSearch() {
 
     }
@@ -79,14 +107,69 @@ public class Controller {
     @FXML
     void findInstructorSfq() {
     	buttonInstructorSfq.setDisable(true);
-      // task in Lab5
-      textAreaConsole.setText(textAreaConsole.getText() + "\n" + textfieldSfqUrl.getText());
+        //textAreaConsole.setText(textAreaConsole.getText() + "\n" + testfieldSfqUrl.getText());
     }
 
     @FXML
     void findSfqEnrollCourse() {
 
     }
+
+    @FXML
+    void filterResults() {
+        String[] checked = new String[10];
+        for (int i = 0; i < 10; i++) {
+            checked[i] = "0";
+        }
+        textAreaConsole.setText("");
+        if (AM.isSelected()) {
+            checked[0] = "AM";
+        }
+        if (PM.isSelected()) {
+            checked[1] = "PM";
+        }
+        if (Mon.isSelected()) {
+            checked[2] = "Mon";
+        }
+        if (Tues.isSelected()) {
+            checked[3] = "Tues";
+        }
+        if (Wed.isSelected()) {
+            checked[4] = "Wed";
+        }
+        if (Thrs.isSelected()) {
+            checked[5] = "Thrs";
+        }
+        if (Fri.isSelected()) {
+            checked[6] = "Fri";
+        }
+        if (LT.isSelected()) {
+            checked[7] = "LT";
+        }
+        if (CC.isSelected()) {
+            checked[8] = "CC";
+        }
+        if (NE.isSelected()) {
+            checked[9] = "NE";
+        }
+    }
+
+    @FXML
+    void selectAll() {
+        AM.setSelected(true);
+        PM.setSelected(true);
+        Mon.setSelected(true);
+        Tues.setSelected(true);
+        Wed.setSelected(true);
+        Thrs.setSelected(true);
+        Fri.setSelected(true);
+        Sat.setSelected(true);
+        LT.setSelected(true);
+        CC.setSelected(true);
+        NE.setSelected(true);
+        SelectAll.setText("Deselect All");
+    }
+
 
     @FXML
     void search() {
@@ -115,8 +198,6 @@ public class Controller {
     	randomLabel.setMaxHeight(60);
 
     	ap.getChildren().addAll(randomLabel);
-
-
 
     }
 
